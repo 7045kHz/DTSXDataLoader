3c5d5b1cb20bc044d4725a1c54ba2b767689c92c
7045kHz <7mhz.cw@gmail.com>
2024-08-04 10:32:01 -0400

    Initial setup of xUnit testing

---
+++ DTSXDataLoader.Tests/Models/DtsMapper_Return_IsNotNull.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DTSXDataLoader.Core.Models;
+
+namespace DTSXDataLoader.Tests.Models;
+public class DtsMapper_Return_IsNotNull
+{
+    [Fact]
+    public void DtsMapper_Return_NotNull()
+    {
+        var dtsMapper = new DtsMapper();
+        Assert.NotNull(dtsMapper);
+    }
+    
+}
---
+++ DTSXDataLoader.Tests/Services/NavigationService_Returns_IsNotNull.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DTSXDataLoader.Core.Models;
+using DTSXDataLoader.Core.Service;
+using DTSXDataLoader.Service;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+
+namespace DTSXDataLoader.Tests.Services;
+public class NavigationService_Returns_IsNotNull
+{
+    [Fact]
+    public void NavidationService_Return_NotNull()
+    {
+        var logger = new LoggerFactory().CreateLogger<DocumentProcessingService>();
+        IConfiguration configuration = new ConfigurationBuilder()
+            .SetBasePath(Directory.GetCurrentDirectory())
+            .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true)
+            .AddEnvironmentVariables()
+            .Build();
+        ServiceProvider serviceProvider = new ServiceCollection()
+            .AddLogging()
+            //  .AddSingleton<IDisplayService, DisplayService>()
+            .AddSingleton<IDocumentProcessingService, DocumentProcessingService>()
+            .AddSingleton<ICommandLineService, CommandLineService>()
+            .AddSingleton<IFileService, FileService>()
+            .AddSingleton<IEtlDatabaseService, EtlDatabaseService>()
+            .AddSingleton<INavigationService, NavigationService>()
+            .AddScoped<IConfiguration>(_ => configuration)
+            .AddScoped<ILogger>(_ => logger)
+            .BuildServiceProvider();
+      
+        var navigationService = serviceProvider.GetService<INavigationService>();
+
+
+        Assert.NotNull(navigationService);
+    }
+}
